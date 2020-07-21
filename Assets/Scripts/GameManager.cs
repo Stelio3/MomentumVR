813415aee9bd2f38dfec279783a0cd1e78f54a88
Pablo <pablohernandezjimenez@gmail.com>
2020-07-21 12:21:37 +0200

    adding oculus and first implemented

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+
+    private float timeZero;
+    [SerializeField]
+    private TextMesh textMesh;
+    private float time;
+
+    public static GameManager GetInstance()
+    {
+        return instance;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = Time.realtimeSinceStartup;
+        timeZero = Time.realtimeSinceStartup;
+        if (instance == null)
+            instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = Time.realtimeSinceStartup - timeZero;
+        textMesh.text = "Time: " + (int)((Time.realtimeSinceStartup - timeZero));
+    }
+
+    public void WriteToFile()
+    {
+        string pathToWrite = Application.dataPath + "/resultadosConTime.txt";
+
+        if (!File.Exists(pathToWrite))
+        {
+            File.WriteAllText(pathToWrite, "--RESULTADOS--\n\n");
+        }
+
+        File.AppendAllText(pathToWrite, "He terminado el ejercicio " + SceneManager.GetActiveScene().name + " con un tiempo de " + time + " segundos \n");
+        Application.Quit(); //cambiar a inicio del siguiente ejercicio
+    }
+}
