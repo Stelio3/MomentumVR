24734583ca30b4761b38e87336811acbf132374f
Pablo <pablohernandezjimenez@gmail.com>
2020-07-27 18:44:29 +0100

    merged all proyect

--- Assets/MomentumVR/Scripts/GameManager.cs
+++ Assets/MomentumVR/Scripts/GameManager.cs
@@ -1,19 +1,38 @@
-﻿using System.Collections;
+﻿using Oculus.Platform.Samples.VrHoops;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 
+
 public class GameManager : MonoBehaviour
 {
     private static GameManager instance;
 
+    [SerializeField]
+    private GameObject prefab;
     private float timeZero;
     [SerializeField]
     private TextMesh textMesh;
-    private float time;
+    [SerializeField]
+    private TextMesh textMeshVelocity;
+    [SerializeField]
+    private TextMesh textMeshPosition;
+    [SerializeField]
+    private TextMesh textMeshAmplitude;
+    [SerializeField]
+    private OVRPlayerController controlador;
 
+    private float time;
+    private float highPos, lowPos;
+    private float lastValueRight, lastValueLeft;
+    private float nextActionTime = 0.0f;
+    public float period = 0.05f;
+    private string fileName,myFilePath, tipoAmplitud, tipoVelocidad,respuesta;
+    private string texto;
+    private string[] lineas;
     public static GameManager GetInstance()
     {
         return instance;
@@ -22,17 +41,116 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        tipoAmplitud = "MEDIA";
         time = Time.realtimeSinceStartup;
         timeZero = Time.realtimeSinceStartup;
         if (instance == null)
             instance = this;
+        highPos = 0.0f;
+        lowPos = 0.0f;
+        lastValueRight = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch)[1];
+        lastValueLeft = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch)[1];
+        fileName = "ejercicios.txt";
+        myFilePath = "/sdcard/Download/" + fileName;
+        
+        string pathToWrite = "/sdcard/Download/" + "resultados.txt";
+
+        if (!File.Exists(pathToWrite))
+        {
+            //File.WriteAllText(pathToWrite, "--RESULTADOS--\n\n");
+            textMeshPosition.text = tipoVelocidad + " / " + respuesta + "/" + tipoAmplitud + "/" + File.Exists(myFilePath);
+        }
+
+
+
+        if (File.Exists(myFilePath))
+        {
+            //texto = File.ReadAllText(myFilePath);
+            textMeshPosition.text = tipoVelocidad + " / " + respuesta + "/" + tipoAmplitud + "/" + File.Exists(myFilePath);
+
+        }
+
+        if (lineas!=null)
+        {
+            foreach (string line in lineas)
+            {
+                string[] words = line.Split(':');
+                if (words[0] == "VUELO")
+                {
+                    string[] parametros = words[1].Split('/');
+                    respuesta = parametros[0];
+                    tipoAmplitud = parametros[1];
+                    tipoVelocidad = parametros[2];
+                }
+                
+            }
+        }
+        
+
+        for (int i = 10; i < 300; i+=30)
+        {
+            switch (tipoAmplitud)
+            {
+                case "BAJA":            
+                    Instantiate(prefab, new Vector3(0, Random.Range(5.0f, 10.0f), i), Quaternion.identity);
+                    break;
+                case "MEDIA":
+                    Instantiate(prefab, new Vector3(0, Random.Range(5.0f, 25.0f), i), Quaternion.identity);
+                    break;
+                case "ALTA":
+                    Instantiate(prefab, new Vector3(0, Random.Range(5.0f, 40.0f), i), Quaternion.identity);
+                    break;
+                default:
+                    Instantiate(prefab, new Vector3(0, Random.Range(5.0f, 30.0f), i), Quaternion.identity);
+                    break;
+            }
+            
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        tipoAmplitud = "MEDIA";
         time = Time.realtimeSinceStartup - timeZero;
         textMesh.text = "Time: " + (int)((Time.realtimeSinceStartup - timeZero));
+        textMeshVelocity.text = "Velocity : " + Mathf.Abs(OVRInput.GetLocalControllerVelocity(OVRInput.Controller.RHand)[1]);
+        //textMeshVelocity.text = "Texto : " + texto;
+        //textMeshPosition.text = "Posicion : " + controlador.transform.position.x + "," + controlador.transform.position.y + "," + controlador.transform.position.z;
+        textMeshPosition.text = tipoVelocidad + " / " + respuesta + "/" + tipoAmplitud + "/" + File.Exists(myFilePath);
+
+        float amplitudeFrameRight = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch)[1] - lastValueRight;
+        lastValueRight = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch)[1];
+        
+        
+        float amplitudeFrameLeft = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch)[1] - lastValueLeft;
+        lastValueLeft = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch)[1];
+
+        controlador.JumpForce = ((Mathf.Abs(amplitudeFrameRight) + Mathf.Abs(amplitudeFrameLeft))/2);
+        //controlador.JumpForce = Mathf.Abs(amplitudeFrameRight);
+        controlador.JumpModified();
+
+        textMeshAmplitude.text = "Amplitud : " + amplitudeFrameRight;
+
+
+        if (OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch)[1] > highPos)
+            highPos = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch)[1];
+        if (OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch)[1] < lowPos)
+            lowPos = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch)[1];
+
+        //desplazamiento hacia adelante
+        Vector3 position = controlador.transform.position;
+        position[2] -= ((Mathf.Abs(OVRInput.GetLocalControllerVelocity(OVRInput.Controller.LHand)[1])+ Mathf.Abs(OVRInput.GetLocalControllerVelocity(OVRInput.Controller.RHand)[1])) * 0.05f)/4.0f;
+        controlador.transform.position = position;
+
+        //fuerza hacia abajo
+        float deltaTime = -0.0005f;
+        //position = controlador.transform.position;
+        controlador.JumpForce = deltaTime;
+        controlador.JumpModified();
+       
+        //position[2] *= Mathf.Abs(OVRInput.GetLocalControllerVelocity(OVRInput.Controller.RHand)[1]) * -0.05f;
+        //controlador.transform.position = position;
     }
 
     public void WriteToFile()
---
+++ Assets/MomentumVR/Scripts/collisionDetector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionDetector : MonoBehaviour
+{
+    // Update is called once per frame
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(this.gameObject);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(this.gameObject);
+    }
+}
