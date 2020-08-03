bef9ac27c80ef39ad84100dc884b8552c58c8df1
Pablo <pablohernandezjimenez@gmail.com>
2020-08-03 15:36:00 +0100

    creating crossed arms game

--- Assets/MomentumVR/Scripts/GameTime.cs
+++ Assets/MomentumVR/Scripts/GameTime.cs
@@ -3,26 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GameTime : MonoBehaviour {
-
+public class GameTime : MonoBehaviour
+{
     public float time;
     private bool timeEnd = true;
+    public string prevText;
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
         time -= Time.deltaTime;
 
-        int timeCorrect= (int)time;
+        int timeCorrect = (int)time;
         if (timeCorrect > 0)
         {
-            GetComponent<Text>().text = "Time to start: " + timeCorrect.ToString().PadLeft(2, '0');
+            GetComponent<Text>().text = prevText + timeCorrect.ToString().PadLeft(2, '0');
         }
-        else if(timeEnd)
+        else if (timeEnd)
         {
             timeEnd = false;
             time = 0;
-            GetComponent<Text>().text = "Time to start: " + timeCorrect.ToString().PadLeft(2, '0');
-            GameManager.Instance.GameStart();
+            GetComponent<Text>().text = prevText + timeCorrect.ToString().PadLeft(2, '0');
         }
     }
 }
