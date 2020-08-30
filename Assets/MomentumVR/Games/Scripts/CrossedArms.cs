152514801863120f5327a10e02a823cbeeb92a13
Pablo <pablohernandezjimenez@gmail.com>
2020-08-30 22:39:51 +0100

    ending the crossedarmsgame

--- Assets/MomentumVR/Games/Scripts/CrossedArms.cs
+++ Assets/MomentumVR/Games/Scripts/CrossedArms.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CrossedArms : MonoBehaviour
+public class CrossedArms : Singleton<CrossedArms>
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    Transform[] BallPositions, ZonesPositions;
 
-    // Update is called once per frame
-    void Update()
+    private int ballPosition, zonePosition;
+    public void Spanw(GameObject ball, GameObject zone)
     {
-        
+        ballPosition = Random.Range(0, BallPositions.Length);
+        zonePosition = Random.Range(0, ZonesPositions.Length);
+        while (ballPosition == zonePosition)
+        {
+            zonePosition = Random.Range(0, ZonesPositions.Length);
+        }
+        Instantiate(ball, BallPositions[ballPosition]);
+        Instantiate(zone, ZonesPositions[zonePosition]);
     }
 }
---
+++ Assets/MomentumVR/Games/Scripts/Moto.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moto : MonoBehaviour
+{
+    private Vector3 direction, velocity;
+    private float leftRotation, rightRotation;
+
+    public GameObject leftHand, rightHand, eje;
+    
+    [SerializeField]
+    float moveSpeed;
+    // Update is called once per frame
+    void Update()
+    {
+        leftRotation = leftHand.transform.rotation.x;
+        rightRotation = rightHand.transform.rotation.x;
+        moveSpeed += leftRotation*0.1f + rightRotation * 0.1f;
+
+        transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
+    }
+}
