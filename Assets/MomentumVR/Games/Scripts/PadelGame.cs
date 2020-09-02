060db81a6d263eb17a7d94311b4cadef621984b6
Kiran <kiran.lorenzo@gmail.com>
2020-09-02 22:49:30 +0200

    Padel Game

---
+++ Assets/MomentumVR/Games/Scripts/PadelGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PadelGame : MonoBehaviour
+{
+    private Rigidbody rBody;
+    public GameObject rHand, lHand;
+    public float ballForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody.AddForce(transform.forward * ballForce);
+        rBody = GetComponent<Rigidbody>();
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == rHand || other.gameObject == lHand)
+        {
+            rBody.AddForce(transform.forward * ballForce);
+            
+        }
+    }
+}
--- Assets/MomentumVR/Games/Scripts/collisionDetector.cs
+++ Assets/MomentumVR/Games/Scripts/collisionDetector.cs
@@ -17,3 +17,4 @@ public class collisionDetector : MonoBehaviour
         Destroy(this.gameObject);
     }
 }
+ 
