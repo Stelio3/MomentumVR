ff66288751e042fefba7ec9db1512b98b96eb3d3
Kiran <kiran.lorenzo@gmail.com>
2020-09-19 10:52:47 +0200

    Estanterías Scene

--- Assets/MomentumVR/Games/Scripts/PadelBall.cs
+++ Assets/MomentumVR/Games/Scripts/PadelBall.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PadelBall : MonoBehaviour
 {
     Vector3 iniPosition;
+    public string hitter;
 
+    int playerScore;
+
+    public bool playing = true;
+
+    [SerializeField] Text playerScoreText;
     // Start is called before the first frame update
     void Start()
     {
         iniPosition = transform.position;
+        playerScore = 0;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -21,5 +29,27 @@ public class PadelBall : MonoBehaviour
 
             GameObject.Find("player").GetComponent<PadelPlayer>().Reset();
         }
+
+        if(playing)
+        {
+            playing = false;
+            updateScore(); 
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if ((other.CompareTag("Player")) && playing)
+        {
+            playerScore++;
+        }
+        playing = false;
+        updateScore();
+    }
+
+    void updateScore()
+    {
+        playerScoreText.text = "Player: " + playerScore;
     }
 }
--- Assets/MomentumVR/Games/Scripts/PadelPlayer.cs
+++ Assets/MomentumVR/Games/Scripts/PadelPlayer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PadelPlayer : MonoBehaviour
 {
     public Transform aimTarget;
+    public Transform ball;
     float speed = 4f;
     float force = 18;
     bool hitting;
@@ -30,7 +31,7 @@ public class PadelPlayer : MonoBehaviour
         {
             aimTarget.Translate(new Vector3(-h, 0, 0) * speed * 2 * Time.deltaTime);
         }
-
+            
 
         if ((h != 0 || v != 0) && !hitting)
         {
@@ -44,6 +45,9 @@ public class PadelPlayer : MonoBehaviour
         {
             Vector3 dir = aimTarget.position - transform.position;
             other.GetComponent<Rigidbody>().velocity = dir.normalized * force + new Vector3(0, 12, 0);
+
+            ball.GetComponent<PadelBall>().hitter = "player";
+            ball.GetComponent<PadelBall>().playing = true;
         }
     }
 
